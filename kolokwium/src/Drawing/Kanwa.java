dd2a82474119329542cc6128d291858773572936
robloxDigital <107726289+robloxDigital@users.noreply.github.com>
2024-01-15 15:30:19 +0100

    added kanwa class, which you can draw on.

---
+++ kolokwium/src/Drawing/Kanwa.java
@@ -0,0 +1,12 @@
+package Drawing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Kanwa extends JPanel {
+    public Kanwa(){
+        setBackground(Color.YELLOW);
+        setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        setLayout(null);
+    }
+}
--- kolokwium/src/MainFrame/MainFrame.java
+++ kolokwium/src/MainFrame/MainFrame.java
@@ -1,10 +1,23 @@
 package MainFrame;
 
+import Drawing.Kanwa;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    private int WIDTH = 800;
+    private int HEIGHT = 600;
     public MainFrame(){
         super("Opadające kwadraty");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
 
+        JPanel mainPanel = new JPanel();
+        setContentPane(mainPanel);
+        mainPanel.setLayout(null);
+
+        Kanwa kanwa = new Kanwa();
+        kanwa.setBounds(0, 0, WIDTH, HEIGHT);
+        mainPanel.add(kanwa);
     }
 }
